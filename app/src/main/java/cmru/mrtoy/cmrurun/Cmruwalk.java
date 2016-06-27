4257cf9a9e3aeae5a45865a3cbf2cbe614be1339
mrtoypt <cm2cars2555@gmail.com>
2016-06-27 11:33:27 +0700

    Update 255906271133

---
+++ app/src/main/java/cmru/mrtoy/cmrurun/Cmruwalk.java
@@ -0,0 +1,13 @@
+package cmru.mrtoy.cmrurun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cmruwalk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cmruwalk);
+    }
+}
