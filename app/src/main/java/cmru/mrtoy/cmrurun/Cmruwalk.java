553c55960cce729863db339e06adddabecbbfcc7
mrtoypt <cm2cars2555@gmail.com>
2016-06-27 13:57:45 +0700

    Update 25590627 Open New Page

--- app/src/main/java/cmru/mrtoy/cmrurun/Cmruwalk.java
+++ app/src/main/java/cmru/mrtoy/cmrurun/Cmruwalk.java
@@ -1,13 +1,19 @@
 package cmru.mrtoy.cmrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Cmruwalk extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) { // method
+        super.onCreate(savedInstanceState);   // statement
         setContentView(R.layout.activity_cmruwalk);
+    }
+
+    public void clickSignUpMain(View view) {          // method no return
+        startActivity(new Intent(Cmruwalk.this,SignUpMainActivity.class));
     }
 }
