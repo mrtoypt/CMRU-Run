1f60e8f29464629fb4c49ef82cef47f1a9e50c52
mrtoypt <cm2cars2555@gmail.com>
2016-06-27 16:35:11 +0700

    End of 27/06/59

---
+++ app/src/main/java/cmru/mrtoy/cmrurun/MyAlert.java
@@ -0,0 +1,26 @@
+package cmru.mrtoy.cmrurun;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by MrTOYPT on 27/6/2559.
+ */
+public class MyAlert {
+    public void myDialog(Context context ,String strTitle ,String strMessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.danger);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+    }
+
+}
--- app/src/main/java/cmru/mrtoy/cmrurun/SignUpMainActivity.java
+++ app/src/main/java/cmru/mrtoy/cmrurun/SignUpMainActivity.java
@@ -2,12 +2,39 @@ package cmru.mrtoy.cmrurun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SignUpMainActivity extends AppCompatActivity {
+
+    // Explicit
+    private EditText nameEditText , userEditText ,passwdEditText ;
+    private RadioGroup radioGroup;
+    private RadioButton avata1RadioButton ,avata2RadioButton ,avata3RadioButton ,avata4RadioButton , avata5RadioButton;
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_main);
+
+        // Bind Widget การผูกตัวแปรกับ Wedget
+        nameEditText = (EditText) findViewById(R.id.editText); // nameEditText = findViewById(R.id.editText) + Alt + Enter = cat
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwdEditText = (EditText) findViewById(R.id.editText2);
+        radioGroup = (RadioGroup) findViewById(R.id.radAvata);
+        avata1RadioButton = (RadioButton) findViewById(R.id.radioButton1);
+        avata2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        avata3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        avata4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+        avata5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
+
     } // end main method
 } // end main class
