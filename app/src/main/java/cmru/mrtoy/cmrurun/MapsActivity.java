7ce5173375aa8cf418b9eb65c614002a364fffe0
mrtoypt <cm2cars2555@gmail.com>
2016-06-29 14:33:23 +0700

    Add Station Comp

--- app/src/main/java/cmru/mrtoy/cmrurun/MapsActivity.java
+++ app/src/main/java/cmru/mrtoy/cmrurun/MapsActivity.java
@@ -7,40 +7,61 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double centralLatADouble = 18.8067293, centralLngADouble = 99.0172408;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.my_layout);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+    } // end of main
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //set position
+        LatLng latLng = new LatLng(centralLatADouble, centralLngADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        creatStationMarker();
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-}
+        //  LatLng sydney = new LatLng(-34, 151);
+        //  mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }// end of onMapReady
+
+    private void creatStationMarker() {
+
+        MyData myData = new MyData();
+        double[] latDoubles = myData.getLatStationDoubles();
+        double[] lngDoubles = myData.getLngStationDoubles();
+        int[] iconInts = myData.getIconStationInts();
+
+
+
+        for (int i = 0; i < latDoubles.length; i++) {
+            LatLng latLng = new LatLng(latDoubles[i], lngDoubles[i]);
+            mMap.addMarker(new MarkerOptions().position(latLng)
+                    .icon(BitmapDescriptorFactory.fromResource(iconInts[i]))
+            .title("มุมที่ " + Integer.toString(i+1)));
+        }//for
+
+
+
+    } // end creatStationMarker
+
+
+} // main
--- app/src/main/java/cmru/mrtoy/cmrurun/MyData.java
+++ app/src/main/java/cmru/mrtoy/cmrurun/MyData.java
@@ -7,11 +7,29 @@ public class MyData {
     // Explicit
     private int[] avataInts = new int[]{20,R.drawable.bird48 , R.drawable.doremon48 ,
     R.drawable.kon48 ,R.drawable.nobita48 ,R.drawable.rat48};
+    private double[] latStationDoubles = new double[]{18.808869, 18.808788, 18.805885, 18.806241};
+    private double[] lngStationDoubles = new double[]{99.017871, 99.018768, 99.018228, 99.017471};
+    private int[] iconStationInts = new int[]{R.drawable.build1, R.drawable.build2,
+            R.drawable.build3, R.drawable.build4};
+
 
 
     // สร้าง Geter โดยการกด Alt + Ins เลือก Getter แล้ว OK
 
     public int[] getAvataInts() {
         return avataInts;
+    }
+
+
+    public double[] getLatStationDoubles() {
+        return latStationDoubles;
+    }
+
+    public double[] getLngStationDoubles() {
+        return lngStationDoubles;
+    }
+
+    public int[] getIconStationInts() {
+        return iconStationInts;
     }
 } // end of MyData Class
