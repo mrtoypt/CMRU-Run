33bee71b2f2272d49b20e7306fe93fdabbb979ff
mrtoypt <cm2cars2555@gmail.com>
2016-06-30 16:11:46 +0700

    Fishnish In Day

--- app/src/main/java/cmru/mrtoy/cmrurun/ExcerciseActivity.java
+++ app/src/main/java/cmru/mrtoy/cmrurun/ExcerciseActivity.java
@@ -1,13 +1,103 @@
 package cmru.mrtoy.cmrurun;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class ExcerciseActivity extends AppCompatActivity {
+
+    //Expercit
+    private String userIDString, nameString, goldString, avataString;
+    private ImageView imageView;
+    private TextView nameTextView, stationTextView, questionTextView;
+    private RadioGroup radioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton, choice4RadioButton;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excercise);
+        // Bind Wiget
+        imageView = (ImageView) findViewById(R.id.imageView7);
+        nameTextView = (TextView) findViewById(R.id.textView7);
+        stationTextView = (TextView) findViewById(R.id.textView8);
+        questionTextView = (TextView) findViewById(R.id.textView9);
+        radioGroup = (RadioGroup) findViewById(R.id.regChoice);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton6);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton7);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton8);
+
+        // Get value form Intent
+        userIDString = getIntent().getStringExtra("userID");
+        nameString = getIntent().getStringExtra("Name");
+        goldString = getIntent().getStringExtra("Gold");
+        avataString = getIntent().getStringExtra("Avata");
+
+        //Show view
+        MyData myData = new MyData();
+        int[] iconInts = myData.getAvataInts();
+        int intIndex = Integer.parseInt(avataString);
+        imageView.setImageResource(iconInts[intIndex]);
+    }// end onCreate
+
+    public void clickAnswer(View view) {
+
+
+    }//end clickAnswer
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Excercise Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://cmru.mrtoy.cmrurun/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Excercise Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://cmru.mrtoy.cmrurun/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
